816e3635838be8a6b986c10cc0988c768d38be37
ice-upm-grupo2 <123931106+ice-upm-grupo2@users.noreply.github.com>
2023-02-01 18:07:25 +0100

    Version con logica de Reles integrada

--- codigo/arduino/pap_arduino.ino
+++ codigo/arduino/pap_arduino.ino
@@ -4,15 +4,23 @@ SoftwareSerial miBT(11,10);
 
 //Variable para leer el valor enviado desde la APP
 char DATO = 0;
-//PIN para Relé de cambio de dirección (Simulación LED Rojo)
-int LEDROJO = 2;
+
 //PIN para intensidad del motor del patinete (Simulación LED Verde)
-int LEDVERDE = 3;
+int MOTOR = 3;
+
+//PINs para la gestión de relés
+int MAESTRO = 2;
+int ESCLAVO_1 = 8;
+int ESCLAVO_2 = 9;
+int WAITTIME = 500;
+bool adelante = true;
+
 //Variables para niveles de intensidad
 int INTENSIDAD_LOW = 75;
 int INTENSIDAD_MEDIUM = 100;
 int INTENSIDAD_HIGH = 250;
 int INTENSIDAD_PARADA = 0;
+
 
 
 void setup() {
@@ -21,39 +29,85 @@ void setup() {
   Serial.println("Ready");
   //Inicializamos la comunicación en serie del dispositivo Bluetooth
   miBT.begin(9600);
-  pinMode(LEDROJO, OUTPUT);
-  pinMode(LEDVERDE, OUTPUT);
+  pinMode(MOTOR, OUTPUT);
+
+  //Inicializamos los pintes para los relés
+  pinMode(MAESTRO, OUTPUT);
+  pinMode(ESCLAVO_1, OUTPUT);
+  pinMode(ESCLAVO_2, OUTPUT);
+
+
+  //Logica inversa para desactivar los relés por defecto. High = Desactivado - Low = Activado
+  digitalWrite(MAESTRO, HIGH);
+  digitalWrite(ESCLAVO_1, HIGH);
+  digitalWrite(ESCLAVO_2, HIGH);
+
 }
 
 void loop() {
+
+  controlMotor();
+  //controlLed();
+  //TEST BORRAR LUEGO
+  //delay(10000);
+  //DATO = 'D';
+}
+
+
+void controlMotor(){
   // Si recibimos algún dato desde el módulo Bluetooth escribimos en el monitor de Serie de Arduino
   if (miBT.available()){
     DATO = miBT.read();
     Serial.write(DATO);
     //Control de relé para cambio de dirección
     if(DATO == 'D'){
-      Serial.write('0');
-      digitalWrite(LEDROJO, !digitalRead(LEDROJO));
+      //Indicaciones para cambiar dirección de motor hacia atrás
+      if(adelante == true){
+        digitalWrite(MAESTRO, LOW);
+        delay(WAITTIME);
+        digitalWrite(ESCLAVO_1, LOW);
+        digitalWrite(ESCLAVO_2, LOW);
+        delay(WAITTIME);
+        digitalWrite(MAESTRO, HIGH);
+        adelante = false;
+      }else{
+      //Indicaciones para cambiar dirección de motor hacia adelante
+        digitalWrite(MAESTRO, LOW);
+        delay(WAITTIME);
+        digitalWrite(ESCLAVO_1, HIGH);
+        digitalWrite(ESCLAVO_2, HIGH);
+        delay(WAITTIME);
+        digitalWrite(MAESTRO, HIGH);
+        adelante = true;
+      }
     }
     //Intensidad baja
     if(DATO == 'L'){
       Serial.write('1');
-      analogWrite(LEDVERDE, INTENSIDAD_LOW);
+      analogWrite(MOTOR, INTENSIDAD_LOW);
     }
     //Intensidad media
     if(DATO == 'M'){
       Serial.write('2');
-      analogWrite(LEDVERDE, INTENSIDAD_MEDIUM);
+      analogWrite(MOTOR, INTENSIDAD_MEDIUM);
     }
     //Intensidad alta
     if(DATO == 'H'){
       Serial.write('3');
-      analogWrite(LEDVERDE, INTENSIDAD_HIGH);
+      analogWrite(MOTOR, INTENSIDAD_HIGH);
     }
     //Detener carrito
     if(DATO == 'S'){
       Serial.write('3');
-      analogWrite(LEDVERDE, INTENSIDAD_PARADA);
+      analogWrite(MOTOR, INTENSIDAD_PARADA);
+      //Para los relés de manera controlada
+      digitalWrite(MAESTRO, LOW);
+      delay(WAITTIME);
+      digitalWrite(ESCLAVO_1, HIGH);
+      digitalWrite(ESCLAVO_2, HIGH);
+      delay(WAITTIME);
+      digitalWrite(MAESTRO, HIGH);
+      adelante = true;
     }
   }
 
@@ -61,4 +115,5 @@ void loop() {
   if (Serial.available()){
     miBT.write(Serial.read());
   }
+
 }
